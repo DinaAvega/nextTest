1c19ff7973a506db731024ae9a5c9abd439ba9d6
Dina_80 <dinaeid@yahoo.com>
2022-06-27 09:58:08 +0200

    first draft next js

---
+++ nextapp/utils/mockData.ts
@@ -0,0 +1,9 @@
+import { User } from '../interfaces'
+
+/** Dummy user data. */
+export const sampleUserData: User[] = [
+  { id: 1000, name: 'Noah' },
+  { id: 2000, name: 'Norah' },
+  { id: 3000, name: 'Adam' },
+  { id: 4000, name: 'Dina' },
+]
